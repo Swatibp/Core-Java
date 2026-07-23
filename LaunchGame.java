ff6c2e5171725b73a32b195a21a9a920da9e3100
Swati Pyatigoudar <107598372+Swatibp@users.noreply.github.com>
2026-07-23 18:24:26 +0530

    Added Java encapsulation examples with BankAccount and Customer classes.

--- LaunchGame.java
+++ LaunchGame.java
@@ -1,25 +1,39 @@
-package org.example;
+package org.encapsulation;
 //Encapsulation
-//private keyword is to make the imporatnt part of code to be inaccessicble from outside.
-// To fetch and set data we are using getter and setter.
-class BankAccount {
-    private float bal;
+class Customer {
+    private int cID;
+    private String cName;
+    private long cNum;
 
-    public void setData(float x) {
-        if (x > 0) { //to prevent negative values
-            bal = x;
+    //Constructor is special type of method that is automatically called when object is created. It is used to initialize state of the object. Constructor which has same name as class and does not have any return tyype not even void.
+    public Customer(int cID, String cName, long cNum) {
+        //Dont use the diff local and instance variable instead use same local
+        // and instance variable in tht case it will print 0, null, 0 (shadowing problem) hence use this keyword.
+//        cID = x;
+//        cName = y;
+//        cNum = z;
+        this.cID = cID;
+        this.cName = cName;
+        this.cNum = cNum;
+    }
+
+    public int getcID() {
+        return cID;
+    }
+
+    public String getcName() {
+        return cName;
+    }
+
+    public long getcNum() {
+        return cNum;
+    }
+}
+    public class CustomerClass {
+        public static void main(String[] args) {
+            Customer c = new Customer(1, "Swati", 8088777777L);
+            System.out.println(c.getcID());
+            System.out.println(c.getcName());
+            System.out.println(c.getcNum());
         }
     }
-
-    public float getData() {
-        return bal;
-    }
-}
-
-public class LaunchGame {
-    public static void main(String[] args) {
-        BankAccount ba = new BankAccount();
-        ba.setData(80884);
-        System.out.println(ba.getData());
-    }
-}
