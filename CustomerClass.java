ad0652da31686f2dbe446636164e1d49a4165386
Swati Pyatigoudar <107598372+Swatibp@users.noreply.github.com>
2026-07-23 18:25:14 +0530

    Added Java encapsulation examples with BankAccount and Customer classes.

--- CustomerClass.java
+++ CustomerClass.java
@@ -1,11 +1,12 @@
-package org.example;
+package org.encapsulation;
 //Encapsulation
 class Customer {
     private int cID;
     private String cName;
     private long cNum;
 
-    public void setData(int cID, String cName, long cNum) {
+    //Constructor is special type of method that is automatically called when object is created. It is used to initialize state of the object. Constructor which has same name as class and does not have any return tyype not even void.
+    public Customer(int cID, String cName, long cNum) {
         //Dont use the diff local and instance variable instead use same local
         // and instance variable in tht case it will print 0, null, 0 (shadowing problem) hence use this keyword.
 //        cID = x;
@@ -30,8 +31,7 @@ class Customer {
 }
     public class CustomerClass {
         public static void main(String[] args) {
-            Customer c = new Customer();
-            c.setData(1, "Rohi", 8088777777L);
+            Customer c = new Customer(1, "Swati", 8088777777L);
             System.out.println(c.getcID());
             System.out.println(c.getcName());
             System.out.println(c.getcNum());
