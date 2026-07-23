7bef29e1cd6e9e683129d9b9a58e76232465f40d
Swati Pyatigoudar <107598372+Swatibp@users.noreply.github.com>
2026-07-23 18:03:28 +0530

    Added Java encapsulation examples with BankAccount and Customer classes.
    Java Encapsulation examples using getters, setters, private variables, and the this keyword.
    Java OOP practice: Encapsulation with getter/setter methods and the this keyword.
    Basic Java programs demonstrating encapsulation, data hiding, getters, setters, and shadowing.

---
+++ CustomerClass.java
@@ -0,0 +1,39 @@
+package org.example;
+//Encapsulation
+class Customer {
+    private int cID;
+    private String cName;
+    private long cNum;
+
+    public void setData(int cID, String cName, long cNum) {
+        //Dont use the diff local and instance variable instead use same local
+        // and instance variable in tht case it will print 0, null, 0 (shadowing problem) hence use this keyword.
+//        cID = x;
+//        cName = y;
+//        cNum = z;
+        this.cID = cID;
+        this.cName = cName;
+        this.cNum = cNum;
+    }
+
+    public int getcID() {
+        return cID;
+    }
+
+    public String getcName() {
+        return cName;
+    }
+
+    public long getcNum() {
+        return cNum;
+    }
+}
+    public class CustomerClass {
+        public static void main(String[] args) {
+            Customer c = new Customer();
+            c.setData(1, "Swati", 8088777777L);
+            System.out.println(c.getcID());
+            System.out.println(c.getcName());
+            System.out.println(c.getcNum());
+        }
+    }
---
+++ LaunchGame.java
@@ -0,0 +1,25 @@
+package org.example;
+//Encapsulation
+//private keyword is to make the imporatnt part of code to be inaccessicble from outside.
+// To fetch and set data we are using getter and setter.
+class BankAccount {
+    private float bal;
+
+    public void setData(float x) {
+        if (x > 0) { //to prevent negative values
+            bal = x;
+        }
+    }
+
+    public float getData() {
+        return bal;
+    }
+}
+
+public class LaunchGame {
+    public static void main(String[] args) {
+        BankAccount ba = new BankAccount();
+        ba.setData(80884);
+        System.out.println(ba.getData());
+    }
+}
